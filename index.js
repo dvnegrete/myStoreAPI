ab0f8b8b9c45e3f831770fc70e3e12436c63c387
Damian Valenzuela Negrete <dvnegrete@gmail.com>
2022-01-13 20:45:13 -0600

    Deploy a Heroku, con ajustes

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const cors = require("cors");
 const routerApi = require("./routes");
 
 const { logErrors, errorHandler, boomErrorHandler } = require("./middlewares/errorHandler");
 
 const app = express();
-const port = 3500;
+const port = process.env.PORT || 3500;
 
 //middleware
 app.use(express.json());
+//aqui definir la lista de dominios que se conectaran a nuestro sitio
+const whitelist = ["http://localhost:8080", "http://myapp.com"];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback( new Error ("no permitido"))
+    }
+  }
+}
+app.use(cors(options))
 
 app.get("/", (req, res) => {
     res.send("Bienvenido a mi servidor en express");
